3358a6bfc7e144299c2ac476bff2481a445842f3
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-21 20:07:48 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //Variáveis e Seleção de elementos
-import API_KEY from "./apikey.js";
 const apiCountryURL = "https://flagsapi.com/"; //BR/flat/64.png
 
 const cityInput = document.querySelector('#city-input');
@@ -14,8 +13,25 @@ const umidityElement = document.querySelector('#umidity span');
 const windElement = document.querySelector('#wind span');
 
 // Funções
-const showWeatherData = (city) => {
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return(data);
+}
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country + "/flat/64.png");
+    umidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed} km/h`;
 }
 
 // Eventos
