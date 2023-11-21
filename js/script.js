1912b2efa7ff3dc2f481ad188bbe10606e1d7143
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-21 19:26:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+//Variáveis e Seleção de elementos
 import API_KEY from "./apikey.js";
+const apiCountryURL = "https://flagsapi.com/"; //BR/flat/64.png
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const cityElement = document.querySelector('#city');
+
+
+// Funções
+const showWeatherData = (city) => {
+
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+    showWeatherData(city);
+});
