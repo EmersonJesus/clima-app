5135794db82542f9c9c8155e14fb5c8b4ffb1918
Emerson Jesus <Emersoncruz73@hotmail.com>
2024-04-26 20:06:12 -0300

    Weather data and background change by city

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Variáveis e Seleção de elementos
-const apiKey = "SUA_API_KEY";
+const apiKey = "75f3e447013b461ab42a51047b4ac8a6";
 const apiCountryURL = "https://flagsapi.com/"; //BR/flat/64.png
 
 const cityInput = document.querySelector('#city-input');
@@ -14,6 +14,7 @@ const umidityElement = document.querySelector('#humidity span');
 const windElement = document.querySelector('#wind span');
 const weatherContainer = document.querySelector('#weather-data');
 
+
 // Funções
 const getWeatherData = async(city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
@@ -27,7 +28,8 @@ const getWeatherData = async(city) => {
 
 const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
-
+    const cityUrl = city.replace(/\s/g, '')
+    
     cityElement.innerText = data.name;
     tempElement.innerText = parseInt(data.main.temp);
     descElement.innerText = data.weather[0].description;
@@ -35,8 +37,15 @@ const showWeatherData = async (city) => {
     countryElement.setAttribute("src", apiCountryURL + data.sys.country + "/flat/64.png");
     umidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed} km/h`;
-
+    
     weatherContainer.classList.remove('hide');
+    changeBackground(`http://source.unsplash.com/random/?${cityUrl}/`);
+}
+
+const changeBackground = (imageUrl) => {
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
 }
 
 // Eventos
